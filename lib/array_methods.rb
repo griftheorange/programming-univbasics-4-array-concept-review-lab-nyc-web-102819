00219b450299e3596e60ffa6d62404fbde328eb5
Griffin Poole <griffinpoole5@gmail.com>
2019-10-07 06:08:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  checker = array[0]
+  array.length.times { |index|
+    if array[index] > checker
+      checker = array[index]
+    end
+  }
 end
 
 def find_min_value(array)
